c9d3d29d0ca6f9c39ba57b18b3c2694d594c16d6
caritoespicaita <caritoespicaita@hotmail.com>
2022-06-06 22:51:37 -0500

    update task 2.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,17 @@
-$( document ).ready(function()  {
-    const dic_id = {}
-    $('input[type=checkbox][data-id={{ amenity.id }}]').change(function() {
-        if ($(this).is(':checked')) {
-            alert(`${this.value} is checked`);
-        }
-        else {
-            alert(`${this.value} is unchecked`);
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    let checkboxes = document.querySelectorAll(".mycheckbox");
+    
+    document.addEventListener('change', function() {
+        let content = [];
+        checkboxes.forEach((checkbox) => {
+            if (checkbox.checked) {
+                content += checkbox.getAttribute('data-name') + ",";
+            }
+        });
+        document.getElementById("text").innerText = content;
     });
+ 
 });
+
+
+    
